17d411fd54e85cc0ac29ac5fc589c6714da212db
17steen <robinoger1207@gmail.com>
2020-12-29 05:07:39 +0100

    added zsh simple config

---
+++ example.zsh
@@ -0,0 +1,26 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+
+schooldir="/mnt/c/Users/Stéén/OneDrive - Enseignement de la Province de Liège/COURS 2EM"
+alias school="cd \"$schooldir\""
+alias chvim="nvim ~/.config/nvim/init.vim"
+alias chzsh="nvim ~/.oh-my-zsh/custom"
+
+alias cmpt="cd ~/Programming/Computer/"
+
+alias vi=nvim
+
+alias mk=make -j8
+
+alias xrdp-start="sudo /etc/init.d/xrdp start"
+alias xrdp-stop="sudo /etc/init.d/xrdp stop"
+
+#source "/home/steen/emsdk/emsdk_env.sh"
